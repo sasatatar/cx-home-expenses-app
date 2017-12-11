12d20bc66d425c71fa6c4d989b8d3fdb9b42bfb9
Sasa Tatar <sasa.tatar@gmail.com>
2017-12-11 15:49:48 +0100

    Add basic Grid with culture sensitive formatting

--- app/routes/log/index.js
+++ app/routes/log/index.js
@@ -1,10 +1,5 @@
 import { Grid, Button, LinkButton, Section } from 'cx/widgets';
-import {computable, bind} from 'cx/ui';
-import {enableMsgBoxAlerts} from 'cx/widgets';
-
-enableMsgBoxAlerts();
-
-import Controller from './Controller';
+import { computable } from 'cx/ui';
 
 import {categoryNames} from '../../data/categories';
 
@@ -12,7 +7,6 @@ export default <cx>
     <h2 putInto="header">Log</h2>
     <Section
         mod="card"
-        controller={Controller}
         style="height: 100%"
         bodyStyle="display: flex; flex-orientation: column"
     >
@@ -46,21 +40,6 @@ export default <cx>
                     format: "currency;;2",
                     align: 'right',
                     sortable: true
-                },
-                {
-
-                    header: 'Actions',
-                    align: 'center',
-                    children: <cx>
-                        <LinkButton mod="hollow" href-tpl="~/entry/{$record.id}">
-                            Edit
-                        </LinkButton>
-                        <Button mod="hollow" 
-                            onClick="remove"
-                            confirm="Are you sure you want to delete this entry?">
-                            Remove
-                        </Button>
-                    </cx>
                 }
             ]}
         />
